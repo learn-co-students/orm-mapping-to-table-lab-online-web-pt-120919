2c851420018ea8ec8b6eba5bd2a67242d92e4417
lydzb123 <lydiabaik@gmail.com>
2020-02-29 18:54:34 -0800

    create a new student object, saves to database

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,7 @@ class Student
   def initialize (name, grade, id=nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
@@ -35,14 +36,15 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
   end
 
 
-
-
-
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+
 end
