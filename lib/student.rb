dfd95d157218e52b7ffb132d486d1612ef4c6946
lydzb123 <lydiabaik@gmail.com>
2020-02-29 18:40:11 -0800

    drops the students table from the database

--- lib/student.rb
+++ lib/student.rb
@@ -13,11 +13,18 @@ class Student
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
-      )
+      );
     SQL
 
     DB[:conn].execute(sql)
+  end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students;
+    SQL
+    
+    DB[:conn].execute(sql)
   end
 
 
