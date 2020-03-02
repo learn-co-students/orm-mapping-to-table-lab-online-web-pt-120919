b65354c7e7e8e3dfd08138711aed1f13f179e8a4
RachelCodesToo <a.r.fausto@gmail.com>
2020-03-02 12:00:31 -0500

    a

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,16 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  attr_reader :id
+  attr_accessor :name, :grade 
+
+  def initialize(name, grade, id = nil)
+    @id = id 
+    @name = name 
+    @grade = grade 
+  end 
+
   
+
+
 end
